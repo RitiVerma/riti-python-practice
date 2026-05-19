a21d3e6168609ec8fa830c7be1c2f6aa56b9fc33
RitiVerma <51124489+RitiVerma@users.noreply.github.com>
2026-05-19 20:06:50 +0530

    add basic python commands

---
+++ basics/first.py
@@ -0,0 +1,33 @@
+print("Hello World");
+print("Welcome to Python Programming");
+
+a = [1,2,'txt',3.14, True]
+
+
+d="python programming"
+
+result = d.capitalize()
+print(result);
+
+a= [1,2,3,4,5,6,7,8,9,10]
+b = [1,2,'txt',3.14, True]
+
+print(a[5])
+print(a[1:2])
+
+a.append(b)
+print(a)
+
+c=(1,2,3,4,5,6,7,8,9,10) # tuple is immutable
+d={1,2,3,4,4,5,5}
+
+print(d)
+
+convert ={
+'a':1,
+'b':2,
+'c':'three'
+} # dictionary
+
+convert['d'] = 4
+print(convert);
